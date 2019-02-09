cfdf4524d1de2b814a8dfaf3c101dcbfbaa515be
Andi Fauzi Akbar <faujiakbar@gmail.com>
2019-02-09 23:11:47 +0700

    change api response

--- app/Helpers/Api.php
+++ app/Helpers/Api.php
@@ -4,7 +4,7 @@ namespace App\Helpers;
 
 Class Api {
   public static function response($s=true,$m=null,$d=[]){
-    return ['status'=>is_bool($s)?1:0,'message'=>is_string($m)?$m:null,'data'=>$d];
+    return ['status'=>is_bool($s)?($s?1:0):($s==1?1:0),'message'=>is_string($m)?$m:null,'data'=>$d];
   }
 }
 
