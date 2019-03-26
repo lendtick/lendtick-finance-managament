3a6dc8922db09672e2bfc7d89706d90b583f0282
Tupay <lutfi@mhs.ubharajaya.ac.id>
2019-03-26 15:52:51 +0700

    keluarin respone

--- app/Http/Controllers/BillerController.php
+++ app/Http/Controllers/BillerController.php
@@ -48,7 +48,7 @@ class BillerController extends Controller {
 			"WORDS"				=> sha1($channel_code . $request_datetime . $shared_key . $login_name)
 		);
 
-		// print_r($send); die();
+		print_r($send); die();
 
 		$res = RestCurl::exec('POST',env('LINK_DOKU_BILLER').'/DepositSystem-api/AgentLoginMIP?',$send);
 		dd($res);
