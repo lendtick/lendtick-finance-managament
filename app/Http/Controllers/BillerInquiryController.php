938ea9a16b29c0ceb6f0b1d6ec1fbf41fd93a2ed
Badai Samoedra <badai.samoedra@yahoo.co.id>
2019-04-07 00:08:37 +0700

    testing billers

--- app/Http/Controllers/BillerInquiryController.php
+++ app/Http/Controllers/BillerInquiryController.php
@@ -84,7 +84,7 @@ class BillerInquiryController extends Controller {
         'CHANNELCODE'       => $channel_code, //Channel Identification Code
         'SESSIONID'         => $request->sessionid, // Session for each success login.
         'REQUESTDATETIME'   => $request_date, // '20190402065223', //$request_date, //yyyyMMddHHmmss
-        'WORDS'             => sha1($channel_code.$request->sessionid.$request_date.env('SHARED_KEY_BILLER').$request->billerid.$request->accountnumber),  // Hashed key combination encryption using SHA1 method. The hashed key generated from combining these parameters in order.
+        'WORDS'             => sha1($channel_code.$request->sessionid.$request_date.env('SHARED_KEY_BILLER').$request->accountnumber.$request->billerid),  // Hashed key combination encryption using SHA1 method. The hashed key generated from combining these parameters in order.
         'BILLERID'          => $request->billerid, // Please refer to BILLER ID LIST
         'ACCOUNT_NUMBER'    => $request->accountnumber,  //PLN POSTPAID Subscriber ID PLN NONTAGLIS Registration Number TELKOM PSTN Area code (4 digit) + Phone number (9 digit, zero left padding) PDAM Customer ID MULTIFINANCE Subscriber ID 
         'SYSTRACE'          => 1000004094, // System trace number
