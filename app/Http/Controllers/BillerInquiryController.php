bcfd0baef9ee56619100337e1096c04a683d95f3
Badai Samoedra <badai.samoedra@yahoo.co.id>
2019-04-06 19:06:14 +0700

    check response

--- app/Http/Controllers/BillerInquiryController.php
+++ app/Http/Controllers/BillerInquiryController.php
@@ -88,14 +88,14 @@ class BillerInquiryController extends Controller {
         'CHANNELCODE'       => $channel_code, //Channel Identification Code
         'SESSIONID'         => $request->sessionid, // Session for each success login.
         'REQUESTDATETIME'   => $request->date, // '20190402065223', //$request_date, //yyyyMMddHHmmss
-        'WORDS'             => sha1($channel_code . $request->sessionid . $request->date . env('SHARED_KEY_BILLER') . $request->billerid . $request->accountnumber),  // Hashed key combination encryption using SHA1 method. The hashed key generated from combining these parameters in order.
+      //   'WORDS'             => sha1($channel_code . $request->sessionid . $request->date . env('SHARED_KEY_BILLER') . $request->billerid . $request->accountnumber),  // Hashed key combination encryption using SHA1 method. The hashed key generated from combining these parameters in order.
         // (CHANNELCODE + SESSIONID + REQUESTDATETIME + SHARED KEY + BILLERID + ACCOUNTNUMBER)
         'BILLERID'          => $request->billerid, // Please refer to BILLER ID LIST
         'ACCOUNT_NUMBER'    => $request->accountnumber,  //PLN POSTPAID Subscriber ID PLN NONTAGLIS Registration Number TELKOM PSTN Area code (4 digit) + Phone number (9 digit, zero left padding) PDAM Customer ID MULTIFINANCE Subscriber ID 
         'SYSTRACE'          => 1000004094, // System trace number
-        'ADDITIONALDATA1'   => $channel_code//,  //Additional information, please fill with channel code
-      //   'ADDITIONALDATA2'   => '', // Additional information 
-      //   'ADDITIONALDATA3'   => '', // Additional information, only BPJS Kesehatan fill this parameter with Phone number and month bill,o i.e "081319422963|2" 
+        'ADDITIONALDATA1'   => $channel_code,  //Additional information, please fill with channel code
+        'ADDITIONALDATA2'   => '', // Additional information 
+        'ADDITIONALDATA3'   => '', // Additional information, only BPJS Kesehatan fill this parameter with Phone number and month bill,o i.e "081319422963|2" 
         );
       //  print_r($check);
       //  $res = (object) RestCurl::exec('POST',env('LINK_DOKU_BILLER').'/DepositSystem-api/Inquiry?',$check);
